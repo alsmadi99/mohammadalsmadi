0adbeb7cb722553ad401074c763ccdde84f2aeed
Mohammad Smadi <smadi.dev@gmail.com>
2023-07-27 17:25:39 +0300

    removed private projects

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -21,39 +21,39 @@ export const socials = [
 ];
 
 export const projects = [
-  {
-    key: "1",
-    content: (
-      <Card
-        imagen={neom}
-        link="https://www.neom.com"
-        title="Neom (OXAGON)"
-        description="A city in neom, oxagon will be a new paradigm where people, industries and technology come together in harmony with nature."
-      />
-    ),
-  },
-  {
-    key: "2",
-    content: (
-      <Card
-        imagen={tamm}
-        link="https://www.tamm.abudhabi/"
-        title="Tamm"
-        description="A unified system helping you find Abu Dhabi government services and information"
-      />
-    ),
-  },
-  {
-    key: "3",
-    content: (
-      <Card
-        imagen={sta}
-        link="https://www.sta.gov.sa"
-        title="Saudi Tourism Authority"
-        description="STA was established to support the growth of the travel and tourism sector by serving the needs of tourism companies and other commercial partners."
-      />
-    ),
-  },
+  // {
+  //   key: "1",
+  //   content: (
+  //     <Card
+  //       imagen={neom}
+  //       link="https://www.neom.com"
+  //       title="Neom (OXAGON)"
+  //       description="A city in neom, oxagon will be a new paradigm where people, industries and technology come together in harmony with nature."
+  //     />
+  //   ),
+  // },
+  // {
+  //   key: "2",
+  //   content: (
+  //     <Card
+  //       imagen={tamm}
+  //       link="https://www.tamm.abudhabi/"
+  //       title="Tamm"
+  //       description="A unified system helping you find Abu Dhabi government services and information"
+  //     />
+  //   ),
+  // },
+  // {
+  //   key: "3",
+  //   content: (
+  //     <Card
+  //       imagen={sta}
+  //       link="https://www.sta.gov.sa"
+  //       title="Saudi Tourism Authority"
+  //       description="STA was established to support the growth of the travel and tourism sector by serving the needs of tourism companies and other commercial partners."
+  //     />
+  //   ),
+  // },
   {
     key: "4",
     content: (
