566626892f379f4539df8cdfa3165575140467e6
Mohammad Alsmadi <alsmadi.work@gmail.com>
2026-08-01 13:31:02 +0400

    test commit, remove later

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,17 @@ const App = () => {
           <main className="xl:w-2/3 sm:w-[90%] xs:w-[100%] mx-auto px-2 md:px-0 flex flex-col justify-center items-center">
             <About />
             <Footer />
+            <script
+              src="https://www.meetmizan.com/embed/mizan-embed.js"
+              data-partner="mohammadalsmadi"
+              data-lang="en"
+              data-currency="USD"
+              data-primary="#123456"
+              data-logo="https://mohammadalsmadi.com/logo.svg"
+              data-cta-label="Give your Zakat"
+              data-cta-url="https://mohammadalsmadi.com/donate"
+              async
+            ></script>
           </main>
         </div>
       </PopoverProvider>
